1e48ab9686ef76af4a810ad6055145888070d621
Sports Social Web <sportssocialweb@Sportss-MacBook-Air-2.local>
2019-05-16 18:41:26 +0530

    Open Arena Ui is updated

--- src/app/global-feed/global-match-feed/global-match-feed.component.ts
+++ src/app/global-feed/global-match-feed/global-match-feed.component.ts
@@ -4,9 +4,13 @@ import { GetService } from '../../shared/services/get.service';
 import { LocationService } from '../../shared/services/location.service';
 
 import { Masonry, MasonryGridItem } from 'ng-masonry-grid';
-import { ISubscription } from 'rxjs/Subscription';
+import { ISubscription, Subscription } from 'rxjs/Subscription';
 import { MatchDataService } from '../../shared/services/match-data.service';
 import { Title, Meta } from '@angular/platform-browser';
+import { Http } from '@angular/http';
+import { Observable } from 'rxjs';
+import { TimeService } from '../../shared/services/time.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'sports-social-global-match-feed',
@@ -19,31 +23,70 @@ export class GlobalMatchFeedComponent implements OnInit, OnDestroy {
   title="Arena | Sports Social";
   metakey="Open Arena,Sports Social,Sports Arena nearby,Sports events nearby,Sports Activities nearby,Sports Grounds nearby, Connect to Sports players nearby,Find Sports players nearby";
   metades="See What's going around you in sports in the open Arena. Use Arena to find,connect,play, follow matches, players, academies, coaches and events in your favorite sport in your locality and around the world | stay connected to your sports world.";
-  Match = [];
+  Matcharr = [];
+
+  posturl='https://test.sportsocial.in/poc/newfeed';
+  urlObj=[{ userid: "69",
+            page: "1",
+            timestamp: "1557567104000",
+            token: "ndsjndsjknjksnndnjkks"
+      }];
   prevPageNo: number = 0;
   nextPageNo: number = 0;
   _masonry: Masonry;
   private _removeAllSubscription: ISubscription;
   private _removeItemSubscription: ISubscription;
   private _removeFirstItemSubscription: ISubscription;
+
+  public showloader: boolean = false; 
+  private subscription: Subscription;
+  private timer: Observable<any>;
   constructor(
     private renderer: Renderer2,
     private matchData: MatchDataService,
     private pagetitle:Title,
-    private metaservice:Meta
+    private metaservice:Meta,
+    private http:Http,
+    private time:TimeService,
+    private router: Router,
   ) { }
 
-  globalMatchFeed( pageNo ) {
-    this.matchData.globalMatchFeed( this.nextPageNo, 0 ).then( (match) => {
-      this.Match = this.Match.concat(match);
-    }).catch( (err) => {
-      console.log(err);
-    });
+  // globalMatchFeed( pageNo ) {
+  //   this.matchData.globalMatchFeed( this.nextPageNo, 0 ).then( (match) => {
+  //     this.Match = this.Match.concat(match);
+  //   }).catch( (err) => {
+  //     console.log(err);
+  //   });
+  // }
+
+  openarenamatches(){
+    this.http.post(this.posturl,this.urlObj)
+    .subscribe(res=>{
+      var body=res.json();
+      console.log("hi this is my console",body);
+      for(const i in body){
+        this.Matcharr.push({
+          feedid:body[i].feedid,
+          userName:body[i].user_name,
+          coverpic:body[i].CoverPic,
+          gamename:body[i].gamename,
+          startdatetime: this.time.timePassed( body[i].startdatetime),
+          InsertedDate:this.time.timePassed(body[i].InsertedDate),
+          Uniquename:body[i].Uniquename,
+          city:body[i].city,
+          Venue_Name:body[i].Venue_Name,
+          MatchStarterUniqueName:body[i].MatchStarterUniqueName,
+          Team1Name:body[i].Team1Name,
+          Team2Name:body[i].Team2Name
+        });
+        console.log("this is feed id:",this.Matcharr[i].coverpic);
+      }
+    })
   }
 
   nextPage(pageNo) {
     this.prevPageNo = pageNo;
-    this.globalMatchFeed(pageNo);
+    // this.globalMatchFeed(pageNo);
   }
 
   @HostListener('window:scroll', ['$event'])onWindowScroll(event) {
@@ -56,8 +99,13 @@ export class GlobalMatchFeedComponent implements OnInit, OnDestroy {
     }
   }
 
+  openAppDownloadPopup() {
+    this.router.navigate( [ { outlets: { 'AppDownload': ['PopUp'] }} ], { skipLocationChange: true });
+  }
+
   ngOnInit() {
-    this.globalMatchFeed(this.nextPageNo);
+    // this.globalMatchFeed(this.nextPageNo);
+    this.openarenamatches();
     this.pagetitle.setTitle(this.title);
     this.metaservice.updateTag({name:'title',content:this.title});
     this.metaservice.updateTag({name:'keywords',content:this.metakey});
