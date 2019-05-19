bdaa1a2a89695b1432fecedf3558485a7f7fe88c
vikky-noelle <vikrant.attri2017@gmail.com>
2019-05-19 17:06:11 +0530

    i cant seem to find the subtopic routes component as it takes me to a different page where css is different and the .ts file is empty but i am still pushing the changes for them to be reviewed once

--- src/app/help-center/help-center.module.ts
+++ src/app/help-center/help-center.module.ts
@@ -3,7 +3,6 @@ import { CommonModule } from '@angular/common';
 import { HelpCenterComponent } from './help-center.component';
 import { HelpCenterContentComponent } from './help-center-content/help-center-content.component';
 import { HelpCenterHeaderComponent } from './help-center-header/help-center-header.component';
-import { FooterComponent } from '../footer/footer.component';
 import { FooterModule } from '../footer/footer.module';
 import { RouterModule } from '@angular/router';
 import { HeaderModule } from '../header/header.module';
