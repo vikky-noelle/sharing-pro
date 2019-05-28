891dad0e789deec2096f297e9a4f281199255d9d
aniketsportsocial <aniket@sportsocial.com>
2019-05-28 17:39:03 +0530

    Changes

--- dist/server/main.js
+++ dist/server/main.js
@@ -6874,12 +6874,12 @@ var OurteamComponent = /** @class */ (function () {
         this.Sourabhdesc = "Sourabh handels the frontend developement at Sport Social,where he oversees the company's goal, including functionality of website & he actively involved in other departments of company.Sourabh holds a Masters in Computer application from National institute of technology.";
         this.Tushardesc = "Tushar Gangvar co-founded of Sporr Social Inc. in 2015 and returned as chief executiv officer in September 2016.Additionaly.Tushar Co-founder square, Where he serves as CEO and chairman";
         this.Nikhildesc = "Nikhil Kanujiya co-founded of Sporr Social Inc. in 2015 and returned as chief executiv officer in September 2016.Additionaly.Nikhil Co-founder square, Where he serves as CEO and chairman";
-        this.Aniketdesc = "Aniket Chaturvedi is the Tech Lead for Sport Social.He is a Full Stack Developer with vested interests in technology.A software engineer, responsible for leading a development team, and responsible for the quality of its technical deliverables.";
+        //Aniketdesc="Aniket Chaturvedi is the Tech Lead for Sport Social.He is a Full Stack Developer with vested interests in technology.A software engineer, responsible for leading a development team, and responsible for the quality of its technical deliverables."
         this.imgarr = [
             { name: "Sourabh Arora", profile: "Web Developer", desc: this.Sourabhdesc, url: "assets/images/football-min.jpg", fburl: this.sourabhFb },
             { name: "Tushar Gangvar", profile: "CEO", desc: this.Tushardesc, url: "assets/images/football-min.jpg" },
             { name: "Nikhil Kanujiya", profile: "CEO", desc: this.Nikhildesc, url: "assets/images/football-min.jpg" },
-            { name: "Aniket Chaturvedi", profile: "Tech Lead", desc: this.Aniketdesc, url: "assets/images/IMG_20181020_190416.jpg" },
+            //  {name:"Aniket Chaturvedi",profile:"Tech Lead",desc:this.Aniketdesc,url:"assets/images/IMG_20181020_190416.jpg"},
             { url: "assets/images/football-min.jpg" },
             { url: "assets/images/football-min.jpg" },
             { url: "assets/images/football-min.jpg" },
@@ -8371,7 +8371,7 @@ exports.LAZY_MODULE_MAP = { "app/terms-and-condition/terms-and-condition.module#
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! /Users/sportssocialweb/Desktop/SportSocialWebsite/src/main.server.ts */"./src/main.server.ts");
+module.exports = __webpack_require__(/*! /Users/sportssocial/Desktop/SportSocialWebsite/src/main.server.ts */"./src/main.server.ts");
 
 
 /***/ }),
