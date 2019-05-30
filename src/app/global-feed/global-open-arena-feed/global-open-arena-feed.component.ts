0daf45b0d33f2910d9abbf78795c874cd2036e1d
Sports Social Web <sportssocialweb@Sportss-MacBook-Air-2.local>
2019-05-30 19:05:25 +0530

    Home match card update with all game card data

--- src/app/global-feed/global-open-arena-feed/global-open-arena-feed.component.ts
+++ src/app/global-feed/global-open-arena-feed/global-open-arena-feed.component.ts
@@ -25,7 +25,6 @@ import { Router } from '@angular/router';
 })
 export class GlobalOpenArenaFeedComponent implements OnInit, OnDestroy {
 
-
   globalArena = [] ;
   prevPageNo: number = 0;
   nextPageNo: number = 0;
@@ -55,7 +54,7 @@ export class GlobalOpenArenaFeedComponent implements OnInit, OnDestroy {
     const matchPomise = this.matchData.globalMatchFeed( this.nextPageNo, 0 );
     const newsPromise =  this.newsData.globalNewsFeed( this.nextPageNo, 'all');
     Promise.all([matchPomise, newsPromise]).then( (data) => {
-      console.log("this is my data for home",data);
+      // console.log("this is my data for home",data);
       this.globalArena = this.globalArena.concat(data['0']).concat(data['1']);
       console.log(this.globalArena);
     }).catch( (err) => {
@@ -67,6 +66,8 @@ export class GlobalOpenArenaFeedComponent implements OnInit, OnDestroy {
     this.prevPageNo = pageNo;
     // this.globalFeed();
   }
+
+  
 
   openAppDownloadPopup() {
     this.router.navigate( [ { outlets: { 'AppDownload': ['PopUp'] }} ], { skipLocationChange: true });
