31403dc3605dbe857cc533dc41b34bb59fff405b
vikky-noelle <vikrant.attri2017@gmail.com>
2019-07-19 13:58:27 +0530

    locality added

---
+++ src/app/profile/locality/locality.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'sports-social-locality',
+  templateUrl: './locality.component.html',
+  styleUrls: ['./locality.component.css']
+})
+export class LocalityComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
