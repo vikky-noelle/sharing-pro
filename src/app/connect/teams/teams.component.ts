ce3d3cff90dcce8533eb6cb105f908e92e87898a
vikky-noelle <vikrant.attri2017@gmail.com>
2019-09-24 10:58:37 +0530

    work in progress

--- src/app/connect/teams/teams.component.ts
+++ src/app/connect/teams/teams.component.ts
@@ -11,59 +11,61 @@ export class TeamsComponent implements OnInit {
   
   arraypool; //this is used to store all the data from the api in connect for filters
   team = [];
-  filtermenubg = document.getElementsByClassName("filter-menu-bg") as HTMLCollectionOf<HTMLElement>;
-  filtermenu = document.getElementsByClassName("filter-menu") as HTMLCollectionOf<HTMLElement>;
   sportlist=[
-    {id: 1, title: "Aerobics"},                 
-    {id: 3, title: "Archery"},                  
-    {id: 5, title: "Badminton"},                
-    {id: 6, title: "Basketball"},               
-    {id: 7, title: "Biking"},                   
-    {id: 9, title: "Bowling"},                   
-    {id: 10, title: "Boxing"},                       
-    {id: 12, title: "Call of Duty"},             
-    {id: 14, title: "Carrom"},                         
-    {id: 15, title: "Chess"},                          
-    {id: 16, title: "Counter Strike"},           
-    {id: 17, title: "Cricket"},                         
-    {id: 18, title: "Cycling"},                         
-    {id: 21, title: "Fifa"},           
-    {id: 22, title: "Fishing"},                         
-    {id: 23, title: "Football"},                       
-    {id: 24, title: "Golf"},                              
-    {id: 25, title: "Gym"},                     
-    {id: 26, title: "Gymnastics"},                   
-    {id: 27, title: "Handball"},                      
-    {id: 29, title: "Hockey"},                           
-    {id: 30, title: "Horse Riding"},             
-    {id: 33, title: "Kabbadi"},                        
-    {id: 34, title: "Kho Kho"},                         
-    {id: 35, title: "Laser Tag"},                     
-    {id: 36, title: "Lawn Tennis"},                      
-    {id: 37, title: "Long Driving"},                    
-    {id: 39, title: "Martial Arts"},             
-    {id: 42, title: "NFS"},                      
-    {id: 43, title: "Paintball"},                     
-    {id: 45, title: "Poker"},                             
-    {id: 46, title: "Snooker"},                     
-    {id: 48, title: "Touch Rugby"},                 
-    {id: 49, title: "Running"},                         
-    {id: 50, title: "Shooting"},                    
-    {id: 51, title: "Skate Boarding"},              
-    {id: 52, title: "Sky Diving"},                
-    {id: 53, title: "Squash"},        
-    {id: 54, title: "Surfing"},                         
-    {id: 55, title: "Swimming"},               
-    {id: 56, title: "Table Tennis"},               
-    {id: 57, title: "Taekwondo"},                  
-    {id: 58, title: "Teen Patti"},                   
-    {id: 59, title: "Trekking"},      
-    {id: 60, title: "VolleyBall"},                   
-    {id: 62, title: "WeightLifting"},     
-    {id: 64, title: "Wrestling"},                     
-    {id: 65, title: "Yoga"} 
+    {id: 1, title: "Aerobics", status:false},                 
+    {id: 3, title: "Archery", status:false},                  
+    {id: 5, title: "Badminton", status:false},                
+    {id: 6, title: "Basketball", status:false},               
+    {id: 7, title: "Biking", status:false},                   
+    {id: 9, title: "Bowling", status:false},                   
+    {id: 10, title: "Boxing", status:false},                       
+    {id: 12, title: "Call of Duty", status:false},             
+    {id: 14, title: "Carrom", status:false},                         
+    {id: 15, title: "Chess", status:false},                          
+    {id: 16, title: "Counter Strike", status:false},           
+    {id: 17, title: "Cricket", status:false},                         
+    {id: 18, title: "Cycling", status:false},                         
+    {id: 21, title: "Fifa", status:false},           
+    {id: 22, title: "Fishing", status:false},                         
+    {id: 23, title: "Football", status:false},                       
+    {id: 24, title: "Golf", status:false},                              
+    {id: 25, title: "Gym", status:false},                     
+    {id: 26, title: "Gymnastics", status:false},                   
+    {id: 27, title: "Handball", status:false},                      
+    {id: 29, title: "Hockey", status:false},                           
+    {id: 30, title: "Horse Riding", status:false},             
+    {id: 33, title: "Kabbadi", status:false},                        
+    {id: 34, title: "Kho Kho", status:false},                         
+    {id: 35, title: "Laser Tag", status:false},                     
+    {id: 36, title: "Lawn Tennis", status:false},                      
+    {id: 37, title: "Long Driving", status:false},                    
+    {id: 39, title: "Martial Arts", status:false},             
+    {id: 42, title: "NFS", status:false},                      
+    {id: 43, title: "Paintball", status:false},                     
+    {id: 45, title: "Poker", status:false},                             
+    {id: 46, title: "Snooker", status:false},                     
+    {id: 48, title: "Touch Rugby", status:false},                 
+    {id: 49, title: "Running", status:false},                         
+    {id: 50, title: "Shooting", status:false},                    
+    {id: 51, title: "Skate Boarding", status:false},              
+    {id: 52, title: "Sky Diving", status:false},                
+    {id: 53, title: "Squash", status:false},        
+    {id: 54, title: "Surfing", status:false},                         
+    {id: 55, title: "Swimming", status:false},               
+    {id: 56, title: "Table Tennis", status:false},               
+    {id: 57, title: "Taekwondo", status:false},                  
+    {id: 58, title: "Teen Patti", status:false},                   
+    {id: 59, title: "Trekking", status:false},      
+    {id: 60, title: "VolleyBall", status:false},                   
+    {id: 62, title: "WeightLifting", status:false},     
+    {id: 64, title: "Wrestling", status:false},                     
+    {id: 65, title: "Yoga", status:false} 
   ];
 
+  filtermenubg = document.getElementsByClassName("filter-menu-bg") as HTMLCollectionOf<HTMLElement>;
+  filtermenu = document.getElementsByClassName("filter-menu") as HTMLCollectionOf<HTMLElement>;
+  clear = document.getElementsByClassName("input-chkbox") as HTMLCollectionOf<HTMLElement>;
+ 
   constructor(
     private postservice: PostService,
     private route: ActivatedRoute
@@ -121,6 +123,7 @@ export class TeamsComponent implements OnInit {
     }
     console.log(this.team);
   }
+  // showing and hiding the filter menu
   showfiltermenu(){
     console.log("working");
     this.filtermenubg[0].style.display="block";
@@ -130,5 +133,26 @@ export class TeamsComponent implements OnInit {
     console.log("working");
     this.filtermenubg[0].style.display="none";
     this.filtermenu[0].style.right="-1000px";
+  }
+  // checkbox check
+  checkbox(string){
+    console.log("chck working");
+    for(var i=0; i<this.sportlist.length; i++){
+      if(string == this.sportlist[i].title){
+        if(this.sportlist[i].status == false){
+          this.sportlist[i].status = true;
+        }
+        else{
+          this.sportlist[i].status = false;
+        }
+      }
+    }
+    // console.log(this.sportlist);
+  }
+  // clearing chkbox
+  clearfilter(){
+    for(var i=0; i<this.sportlist.length; i++){
+      
+    }
   }
 }
